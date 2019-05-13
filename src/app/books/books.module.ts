0e739e5cf0d90b5b9cfe3f36b517b7a7c1d42463
Peter <baggersio@icloud.com>
2019-05-13 14:13:40 +0200

    feat(task) use *ngFor

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { BooksRoutingModule } from './books-routing.module';
 import { BooksComponent } from './books/books.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
-  declarations: [BooksComponent],
+  declarations: [BooksComponent, BookListComponent],
   imports: [
     CommonModule,
     BooksRoutingModule
   ],
-  exports: [BooksComponent]
+  exports: [BooksComponent, BookListComponent]
 })
 export class BooksModule { }
