55b49e9f43ef6d0c79fd9e11a212a24e405534a3
Peter <baggersio@icloud.com>
2019-05-13 13:28:18 +0200

    feat(task) add a (titleClicked) @Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'books';
+
+  titleClicked(title: string) {
+    console.log(title);
+  }
 }
--- src/app/title-box/title-box.component.ts
+++ src/app/title-box/title-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'title-box',
@@ -9,9 +9,15 @@ export class TitleBoxComponent implements OnInit {
 
   @Input() title: string;
 
+  @Output() titleClicked = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  sendPing() {
+    this.titleClicked.emit(this.title);
   }
 
 }
