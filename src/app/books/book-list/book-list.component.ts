31e9ab0483c54f0dfcd418f3d387bf3b2ff39d04
Peter <baggersio@icloud.com>
2019-05-15 08:04:25 +0200

    feat(task) Component LifeCycle Basic

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Book } from '../book';
 import { BookDataService } from '../book-data.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
 
   books: Book[];
 
-  constructor(private bookData: BookDataService) {
-    this.bookData.getBooks().subscribe(books => this.books = books);
-  }
+  private subscription: Subscription;
+
+  constructor(private bookData: BookDataService) {}
 
   ngOnInit() {
+    this.subscription = this.bookData.getBooks().subscribe(books => this.books = books);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
