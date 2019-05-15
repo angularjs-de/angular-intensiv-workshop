40f585628e168b356a0bafdbcebd4795421cebff
Peter <baggersio@icloud.com>
2019-05-15 07:58:03 +0200

    feat(task) Share BookData service trhough your NgModule

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { BooksRoutingModule } from './books-routing.module';
 import { BooksComponent } from './books/books.component';
 import { BookListComponent } from './book-list/book-list.component';
+import { BookDataService } from './book-data.service';
 
 @NgModule({
   declarations: [BooksComponent, BookListComponent],
   imports: [
     CommonModule,
     BooksRoutingModule
+  ],
+  providers: [
+    BookDataService
   ],
   exports: [BooksComponent, BookListComponent]
 })
